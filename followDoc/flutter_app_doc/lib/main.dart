e591c91a03cf10f1f094e4954dbffc55592a3be4
Porramet Taeng-orn <37680321+porrametict@users.noreply.github.com>
2019-01-13 11:30:20 +0700

    cat can fly

--- followDoc/flutter_app_doc/lib/main.dart
+++ followDoc/flutter_app_doc/lib/main.dart
@@ -8,6 +8,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.deepOrange,
+        accentColor: Colors.deepPurple
+
+      ),
         home: Scaffold(
             appBar: AppBar(
               title: Text('EasyList'),
